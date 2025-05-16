99d4f3295847645e7d758f82d72a6f581ea3aa97
ignasrum <rumbavicius@gmail.com>
2025-05-16 20:09:32 +0200

    config file directory is loaded from environment variables

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,37 @@ mod configuration;
 mod editor;
 mod notebook; // Use the updated notebook module
 
-use std::process::exit;
+use std::env;
+use std::process::exit; // Import the env module
 
 use editor::Editor;
 use iced::Application;
 
 fn main() -> iced::Result {
-    match configuration::read_configuration("./config.json") {
+    // Define the environment variable name to check
+    let config_path_env_var = "COGNATE_CONFIG_PATH";
+    // Define the default configuration file path
+    let default_config_path = "./config.json";
+
+    // Attempt to get the config path from the environment variable
+    let config_path = match env::var(config_path_env_var) {
+        Ok(path) => {
+            println!(
+                "Using configuration path from environment variable {}: {}",
+                config_path_env_var, path
+            );
+            path
+        }
+        Err(_) => {
+            println!(
+                "Environment variable {} not set. Using default path: {}",
+                config_path_env_var, default_config_path
+            );
+            default_config_path.to_string()
+        }
+    };
+
+    match configuration::read_configuration(&config_path) {
         Ok(config) => {
             println!("Theme: {}", config.theme);
             println!("Notebook Path: {}", config.notebook_path); // Print the read notebook path
@@ -25,7 +49,7 @@ fn main() -> iced::Result {
             let _ = Editor::run(settings);
         }
         Err(err) => {
-            eprintln!("Failed to read configuration: {}", err);
+            eprintln!("Failed to read configuration from {}: {}", config_path, err);
             exit(1);
         }
     }
